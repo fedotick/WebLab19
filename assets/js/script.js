16366f91d00a9a52d8de925218060a2e526cdc7b
fedotick <feodortcaciuc2004@gmail.com>
2024-03-28 17:22:36 +0200

    Task 1 updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
 // Task 1
-const btnShowZeros = document.querySelector('#btnShowZeros');
+let isHighlighted = false;
 
-btnShowZeros.addEventListener('click', showZeros);
+const btnHighlightZeros = document.querySelector('#btnShowZeros');
 
-function showZeros() {
+btnShowZeros.addEventListener('click', btnHighlightZerosClick);
+
+function btnHighlightZerosClick() {
     const cells = document.querySelectorAll('#table td');
+    
+    if (isHighlighted) {
+        removeHighlightingZeros(cells);
+        this.textContent = 'show zeros';
+        isHighlighted = false;
+    } else {
+        highlightZeros(cells);
+        this.textContent = 'hide zeros';
+        isHighlighted = true;
+    }
+};
 
-    cells.forEach(cell => {
-        if (cell.textContent == '0') {
-            cell.classList.add('red-cell');
-        } 
-    });
-}
+function highlightZeros(cells) {
+    cells.forEach(cell => highlightZero(cell));
+};
+
+function highlightZero(cell) {
+    cell.textContent === '0' ? cell.classList.add('red-cell') : cell.classList.remove('red-cell');
+};
+
+function removeHighlightingZeros(cells) {
+    cells.forEach(cell => cell.classList.remove('red-cell'));
+};
