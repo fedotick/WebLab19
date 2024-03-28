32ed49f3ed872ee1575eb908d2b7f296a39be5a7
fedotick <feodortcaciuc2004@gmail.com>
2024-03-28 17:31:46 +0200

    Task 2 complited

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,11 @@
 let isHighlighted = false;
 
 const btnHighlightZeros = document.querySelector('#btnShowZeros');
+const cells = document.querySelectorAll('#table td');
 
 btnShowZeros.addEventListener('click', btnHighlightZerosClick);
 
 function btnHighlightZerosClick() {
-    const cells = document.querySelectorAll('#table td');
-    
     if (isHighlighted) {
         removeHighlightingZeros(cells);
         this.textContent = 'show zeros';
@@ -17,16 +16,29 @@ function btnHighlightZerosClick() {
         this.textContent = 'hide zeros';
         isHighlighted = true;
     }
-};
+}
 
 function highlightZeros(cells) {
     cells.forEach(cell => highlightZero(cell));
-};
+}
 
 function highlightZero(cell) {
     cell.textContent === '0' ? cell.classList.add('red-cell') : cell.classList.remove('red-cell');
-};
+}
 
 function removeHighlightingZeros(cells) {
     cells.forEach(cell => cell.classList.remove('red-cell'));
-};
+}
+
+// Task 2
+cells.forEach(cell => cell.addEventListener('click', cellClick));
+
+function cellClick() {
+    const newText = prompt('Enter text:');
+
+    if (newText !== null) {   
+        this.textContent = newText;
+
+        if (isHighlighted) highlightZero(this);
+    }
+}
